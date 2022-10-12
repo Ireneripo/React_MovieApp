4beb31c0d89d106117e6a9c79628eef99e13c9f9
Irene Riportella <riportellairene@gmail.com>
2022-10-12 15:33:49 +0200

    refactored GET request

--- src/App.js
+++ src/App.js
@@ -21,15 +21,18 @@ function App() {
       const data = await response.json();
       console.log(data);
 
-      const transformedMovies = data.results.map((movieData) => {
-        return {
-          id: movieData.episode_id,
-          title: movieData.title,
-          openingText: movieData.opening_crawl,
-          releaseDate: movieData.release_date,
-        };
-      });
-      setMovies(transformedMovies);
+      const loadedMovies = [];
+
+      for (const key in data) {
+        loadedMovies.push({
+          id: key,
+          title: data[key].title,
+          openingText: data[key].openingText,
+          releaseDate: data[key].releaseDate,
+        });
+      }
+
+      setMovies(loadedMovies);
     } catch (error) {
       setError(error.message);
     }
